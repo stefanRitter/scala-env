26eec7b8b95fc39db793f44d8df3a8258416fc5a
stefanRitter <stefan@stefanritter.com>
2013-09-29 11:37:17 +0100

    cleanup

---
+++ ScalaCourse1/.worksheet/src/week02.currying.scala
@@ -0,0 +1,5 @@
+package week02
+
+object currying {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(77); 
+  println("Welcome to the Scala worksheet")}
+}
---
+++ ScalaCourse1/bin/week02/currying.sc
@@ -0,0 +1,5 @@
+package week02
+
+object currying {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
